5f474c865ae4a6d63caead11c8683deae01bbad7
Eric Shankman <ershankman@gmail.com>
2025-05-17 15:42:25 -0400

    feat: updated logic to FAQ, login, profile, and package.json root to add client & server run commands

--- client/src/components/login/login.tsx
+++ client/src/components/login/login.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import type { FormEvent } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
-import styles from './login.module.css';
+// import styles from './login.module.css';
 
 const Login: React.FC = () => {
   const [action, setAction] = useState('Login');
