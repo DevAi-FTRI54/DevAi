183db5763f04bb8c9efdac09ea3fe9479eb9f0b5
kyleheadley1 <kyleantonioheadley@gmail.com>
2025-06-07 16:12:59 -0400

    6/7/4:12

--- client/src/components/auth/orgselector.tsx
+++ client/src/components/auth/orgselector.tsx
@@ -5,14 +5,17 @@ type Org = {
   login: string;
 };
 
-const OrgSelector: React.FC<{ token: string; onSelect: (org: string) => void }> = ({ token, onSelect }) => {
+const OrgSelector: React.FC<{
+  token: string;
+  onSelect: (org: string) => void;
+}> = ({ token, onSelect }) => {
   const [orgs, setOrgs] = useState<Org[]>([]);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchOrgs = async () => {
       if (!token) return;
-      const res = await fetch('https://api.github.com/user/orgs', {
+      const res = await fetch('http://localhost:4000/api/auth/orgs', {
         headers: { Authorization: `Bearer ${token}` },
       });
       const data = await res.json();
@@ -23,10 +26,13 @@ const OrgSelector: React.FC<{ token: string; onSelect: (org: string) => void }> 
   }, [token]);
 
   if (loading) return <div> Loading organizations ...</div>;
-
+  if (orgs.length === 1) {
+    onSelect(orgs[0].login);
+    return null;
+  }
   return (
     <select onChange={(e) => onSelect(e.target.value)}>
-      <option value="">Select Organization</option>
+      <option value=''>Select Organization</option>
       {orgs.map((org) => (
         <option key={org.id} value={org.login}>
           {org.login}
