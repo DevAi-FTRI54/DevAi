d75e289fc8878c5e9dc445ed7f65e7c31ad44a96
kyleheadley1 <kyleantonioheadley@gmail.com>
2025-06-09 17:37:24 -0400

    render test

--- server/src/config/auth.ts
+++ server/src/config/auth.ts
@@ -7,3 +7,7 @@ const __dirname = path.dirname(__filename);
 
 export const GITHUB_APP_PRIVATE_KEY =
   process.env.GITHUB_APP_PRIVATE_KEY?.replace(/\\n/g, '\n');
+
+if (!process.env.GITHUB_APP_PRIVATE_KEY) {
+  throw new Error('GITHUB_APP_PRIVATE_KEY is not set in environment variables');
+}
