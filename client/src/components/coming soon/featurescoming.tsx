023c688fbe7cc07f93c27f6a90ef46fdba7a2378
Eric Shankman <ershankman@gmail.com>
2025-06-11 11:17:15 -0400

    updated for features coming soon pages for solutions & pricing

---
+++ client/src/components/coming soon/featurescoming.tsx
@@ -0,0 +1,15 @@
+const ComingSoon = () => (
+  <div className="relative h-screen w-full flex flex-col items-center justify-center bg-[#171717] overflow-hidden">
+    <div className="flex flex-col items-center z-10 px-4">
+      <div className="text-6xl mb-6">⏳</div>
+      <div className="text-white text-3xl font-semibold mb-2">Stay Tuned!</div>
+      <div className="text-white text-lg opacity-75 text-center">
+        More to come
+        <br />
+        Check back later.
+      </div>
+    </div>
+  </div>
+);
+
+export default ComingSoon;
